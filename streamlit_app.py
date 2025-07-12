7ce59656bd6300b6472ec3700de4c68af38f09c1
jonathandurand <jonathan.durand25@gmail.com>
2025-07-12 10:26:18 +0000

    Test de limiter l'utilisation des ressources en utilisant le cache

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,29 +12,20 @@ import dagshub
 
 import shap
 
-def init():
-    dagshub.auth.add_app_token('7ff59a8ec595a39c81790087b5fe632c13a71e8c')
-    dagshub.init(repo_owner='jonathan.durand25', repo_name='OC_P7', mlflow=True)
-
-    app_train = pd.read_csv('datas/app_train_1.csv')
-    #app_train_2 = pd.read_csv('datas/app_train_2.csv')
-    #app_train_3 = pd.read_csv('datas/app_train_3.csv')
-    #app_train_4 = pd.read_csv('datas/app_train_4.csv')
-    #app_train = pd.concat([app_train_1, app_train_2, app_train_3, app_train_4])
-    print("Lecture données OK")
-
+@st.cache_data
+def app_train_load(csv_file):
+    app_train = pd.read_csv(csv_file)
     app_train['BIRTH_YEARS'] = app_train['DAYS_BIRTH'] / -365
     app_train = app_train.drop('DAYS_BIRTH', axis=1)
 
-    # Create an anomalous flag column
     app_train['DAYS_EMPLOYED_ANOM'] = app_train["DAYS_EMPLOYED"] == 365243
 
-    # Replace the anomalous values with nan
     app_train['DAYS_EMPLOYED'].replace({365243: np.nan})
 
-    st.session_state['app_train'] = app_train
-    print(app_train.shape)
+    return app_train
 
+@st.cache_data
+def data_calcul(app_train):
     app_train_enc = app_train.copy()
 
     # Create a label encoder object
@@ -58,23 +49,48 @@ def init():
 
     app_train_enc = app_train_enc.drop(columns = ['CODE_GENDER_XNA', 'NAME_INCOME_TYPE_Maternity leave', 'NAME_FAMILY_STATUS_Unknown'])
 
-    print('app_train encoded')
-    print(app_train_enc.shape)
-
     data = app_train_enc.drop(columns = ['TARGET'])
 
+    return data
+
+@st.cache_data
+def data_scaledMM_calcul(data):
     scaler_minMax = MinMaxScaler(feature_range = (0, 1))
     scaler_minMax.fit(data);
 
     data_scaledMM = scaler_minMax.transform(data)
     data_scaledMM = pd.DataFrame(data=data_scaledMM, columns = data.columns)
-    st.session_state['data_scaledMM'] = data_scaledMM
+    return data_scaledMM
+
+@st.cache_resource
+def models_load():
+    dagshub.auth.add_app_token('7ff59a8ec595a39c81790087b5fe632c13a71e8c')
+    dagshub.init(repo_owner='jonathan.durand25', repo_name='OC_P7', mlflow=True)
+    model = mlflow.pyfunc.load_model('runs:/4e1e5e9e3f3f48fda5eb52dbc836038c/model')
+    model_learn = mlflow.sklearn.load_model('runs:/fa1ccfb2e0814e3d9261f7098c3d60c9/RF_sel_med')
+    return model, model_learn
+
+
+def init():
+
+    app_train = app_train_load('datas/app_train_1.csv')
+    
+    #app_train_2 = pd.read_csv('datas/app_train_2.csv')
+    #app_train_3 = pd.read_csv('datas/app_train_3.csv')
+    #app_train_4 = pd.read_csv('datas/app_train_4.csv')
+    #app_train = pd.concat([app_train_1, app_train_2, app_train_3, app_train_4])
+    print("Lecture données OK")
+    print(app_train.shape)
+    
+    data = data_calcul(app_train)
+    
+    data_scaledMM = data_scaledMM_calcul(data)
 
     print('data scaled')
     print(data_scaledMM.shape)
 
-    st.session_state['model'] = mlflow.pyfunc.load_model('runs:/4e1e5e9e3f3f48fda5eb52dbc836038c/model')
-    st.session_state['model_learn'] = mlflow.sklearn.load_model('runs:/fa1ccfb2e0814e3d9261f7098c3d60c9/RF_sel_med')
+    model, model_learn = models_load()
+
     st.session_state['threshold'] = 0.56
     print('model loaded')
 
@@ -88,19 +104,21 @@ def init():
     st.session_state['data_sel'] = data[st.session_state['features_sel']]
     print('data sel')
 
-    st.session_state['explainer'] = shap.TreeExplainer(st.session_state['model_learn'])
-    print('shap')
+    #st.session_state['explainer'] = shap.TreeExplainer(st.session_state['model_learn'])
+    #print('shap')
 
 def main():
     st.markdown("# Main page 🎈")
     st.sidebar.markdown("# Main page 🎈")
+
+    app_train = app_train_load('datas/app_train_1.csv')
 
     st.markdown("## Choix du client")
     left, right = st.columns(2, vertical_alignment="bottom")
 
     client = left.selectbox(
         label = "Client",
-        options=st.session_state['app_train']['SK_ID_CURR'].values,
+        options=app_train['SK_ID_CURR'].values,
         index=None,
         placeholder="Sélectionner un client",
         label_visibility="collapsed"
